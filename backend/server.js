76886bb2b144d6d6e5844fe46e017d23ed96b47f
AngelDeLara <robertoangel19@gmail.com>
2024-09-23 20:03:29 -0600

    add router

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
@@ -11,9 +12,8 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/', (req,res) =>{
-  res.json({mssg:'Welcome to the app'})
-})
+// routes
+app.use('/api/workouts', workoutRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log('listening on port', process.env.PORT)
