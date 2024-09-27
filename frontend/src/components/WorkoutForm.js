a57bb61a335d3d04926b91c84acf373c89949c5b
AngelDeLara <robertoangel19@gmail.com>
2024-09-27 02:29:24 -0600

    update empty error

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
   const [load, setLoad] = useState('')
   const [reps, setReps] = useState('')
   const [error, setError] = useState(null)
+  const [emptyFields, setEmptyFields] = useState([])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -24,12 +25,14 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error)
+      setEmptyFields(json.emptyFields)
     }
     if(response.ok){
       setTitle('')
       setLoad('')
       setReps('')
       setError(null)
+      setEmptyFields([])
       console.log('added: ', json)
       dispatch({type: 'CREATE_WORKOUT', payload: json})
     }
@@ -44,13 +47,15 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
       />
 
       <label>Load (in kg):</label>
       <input
         type="number"
         onChange={(e) => setLoad(e.target.value)}
-        value={load}      
+        value={load}
+        className={emptyFields.includes('load') ? 'error' : ''}
       />
 
       <label>Reps:</label>
@@ -58,6 +63,7 @@ const WorkoutForm = () => {
         type="number"
         onChange={e => setReps(e.target.value)}
         value={reps}
+        className={emptyFields.includes('reps') ? 'error' : ''}
       />
 
       <button>Add</button>
