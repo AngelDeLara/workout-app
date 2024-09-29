3368b2ba1c2a6c0e7e60c3a80a601a5cafb6cc49
AngelDeLara <robertoangel19@gmail.com>
2024-09-29 02:51:32 -0600

    test

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
 
+const API_URL = process.env.REACT_APP_API_URL
+
 export const useLogin = () => {
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(null)
@@ -10,7 +12,7 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('https://workout-app-y5lx.onrender.com/api/user/login', {
+    const response = await fetch(`${API_URL}/api/user/login`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({email, password})
